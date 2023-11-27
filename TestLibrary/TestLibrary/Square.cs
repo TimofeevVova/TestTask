208364a2246d318efbfa7620c89ca4ccd1b8be6b
Владимир Тимофеев <energon232@gmail.com>
2023-11-27 16:48:03 +0200

    Тестовое задание

---
+++ TestLibrary/TestLibrary/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class CircleCalculator
+    {
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+    }
+    public class TriangleCalculator
+    {
+        public double CalculateArea(double side1, double side2, double side3)
+        {
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        }
+
+        public bool IsRightAngled(double side1, double side2, double side3)
+        {
+            // Проверка на прямоугольность с использованием теоремы Пифагора
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
---
+++ TestLibrary/TestLibraryTests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using TestLibrary;
+
+namespace LibraryTests
+{
+    [TestFixture]
+    public class CircleCalculatorTests
+    {
+        [Test]
+        public void CalculateArea_ValidRadius_ReturnsCorrectArea()
+        {
+            // Arrange
+            CircleCalculator calculator = new CircleCalculator();
+            double radius = 5;
+
+            // Act
+            double area = calculator.CalculateArea(radius);
+
+            // Assert
+            Assert.That(Math.PI * Math.Pow(radius, 2), Is.EqualTo(area));
+        }
+    }
+
+    [TestFixture]
+    public class TriangleCalculatorTests
+    {
+        [Test]
+        public void CalculateArea_ValidSides_ReturnsCorrectArea()
+        {
+            // Arrange
+            TriangleCalculator calculator = new TriangleCalculator();
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+
+            // Act
+            double area = calculator.CalculateArea(side1, side2, side3);
+
+            // Assert
+            Assert.That(6, Is.EqualTo(area));
+
+        }
+
+        [Test]
+        public void IsRightAngled_ValidSides_ReturnsTrue()
+        {
+            // Arrange
+            TriangleCalculator calculator = new TriangleCalculator();
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+
+            // Act
+            bool isRightAngled = calculator.IsRightAngled(side1, side2, side3);
+
+            // Assert
+            Assert.That(isRightAngled, Is.True);
+        }
+    }
+}
